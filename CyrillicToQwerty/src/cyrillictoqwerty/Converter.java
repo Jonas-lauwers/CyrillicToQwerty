5499743f9d22dca18adf144e717ff8565384f1f5
Jonas-lauwers <jonas.lauwers@gmail.com>
2016-09-20 17:35:40 +0300

    First version of Converter class

--- CyrillicToQwerty/src/cyrillictoqwerty/Converter.java
+++ CyrillicToQwerty/src/cyrillictoqwerty/Converter.java
@@ -5,17 +5,83 @@
  */
 package cyrillictoqwerty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Rasendori
  */
 public class Converter {
+    private Map<Character, Character> conversions;
+    
+    /**
+     * Initiliaze the converter and creates the conversion table
+     * TODO for now the table is created here, but in future will be read out from a file instead.
+     */
+    public Converter() {
+        conversions = new HashMap<>();
+        conversions.put('ё', '`');
+        conversions.put('й', 'q');
+        conversions.put('ц', 'w');
+        conversions.put('у', 'e');
+        conversions.put('к', 'r');
+        conversions.put('е', 't');
+        conversions.put('н', 'y');
+        conversions.put('г', 'u');
+        conversions.put('ш', 'i');
+        conversions.put('щ', 'o');
+        conversions.put('з', 'p');
+        conversions.put('х', '[');
+        conversions.put('ъ', ']');
+        conversions.put('ф', 'a');
+        conversions.put('ы', 's');
+        conversions.put('в', 'd');
+        conversions.put('а', 'f');
+        conversions.put('п', 'g');
+        conversions.put('р', 'h');
+        conversions.put('о', 'j');
+        conversions.put('л', 'k');
+        conversions.put('д', 'l');
+        conversions.put('ж', ';');
+        conversions.put('э', '\'');
+        conversions.put('я', 'z');
+        conversions.put('ч', 'x');
+        conversions.put('с', 'c');
+        conversions.put('м', 'v');
+        conversions.put('и', 'b');
+        conversions.put('т', 'n');
+        conversions.put('ь', 'm');
+        conversions.put('б', ',');
+        conversions.put('ю', '.');
+        
+    }
 
     /**
-     * @param args the command line arguments
+     * Converts cyrillic character to latin character thats in the same position on the keyboard
+     * @param input Cyrillic character
+     * @return latin character
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private char convertChar(char input) {
+        return conversions.get(input);
     }
     
+    public String convert(String input) {
+        StringBuilder output = new StringBuilder();
+        for(char c : input.toCharArray()) {
+            boolean isUpper = false;
+            if(Character.isUpperCase(c)) {
+                isUpper = true;
+                c = Character.toLowerCase(c);
+            }
+            if(conversions.containsKey(c)) {
+                c = convertChar(c);
+            }
+            if(isUpper){
+                c = Character.toUpperCase(c);
+            }
+            output.append(c);
+        }
+        return output.toString();
+    }
 }
